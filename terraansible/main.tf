e24e07210b57ce7cd3644e7bc726c650d88b84e3
root <root@shinoby2.mylabserver.com>
2018-10-02 14:02:53 +0000

    This is a new commit for what I originally planned to be amended

---
+++ terraansible/main.tf
@@ -0,0 +1,230 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+#data "aws_availability_zones" "available" {}
+
+#------------IAM---------------- 
+
+#S3_access
+
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name = "s3_access"
+  role = "${aws_iam_role.s3_access_role.name}"
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+  },
+      "Effect": "Allow",
+      "Sid": ""
+      }
+    ]
+}
+EOF
+}
+
+
+#------------------ VPC ---------------------
+
+resource "aws_vpc" "wp_vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support    = true
+
+  tags {
+    Name = "wp_vpc"
+   }
+}
+
+# internet gateway
+
+resource "aws_internet_gateway" "wp_internet_gateway" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name = "wp_igw"
+  }
+}
+
+# Route tables
+
+resource "aws_route_table" "wp_public_rt" { 
+ vpc_id = "${aws_vpc.wp_vpc.id}"
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+ }
+
+ tags {
+   Name = "wp_public"
+  }
+}
+
+resource "aws_default_route_table" "wp_private_rt" {
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+    Name = "wp_private"
+  }
+}
+
+#Subenets
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+ 
+  tags {
+    Name = "wp_public2"
+  }
+}
+
+
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private1"
+  }
+}
+
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private2"
+  }
+}
+
+resource "aws_subnet" "wp_private3_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private3"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private3"
+  }
+}
+
+resource "aws_subnet" "wp_private4_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private4"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private4"
+  }
+}
+
+resource "aws_subnet" "wp_private5_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private5"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private5"
+  }
+}
+
+resource "aws_subnet" "wp_private6_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private6"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private6"
+  }
+}
+
+resource "aws_subnet" "wp_oracle1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["oracle1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_oracle1"
+  }
+}
+
+resource "aws_subnet" "wp_oracle2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["oracle2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_oracle2"
+  }
+}
+
+#Internet NAT GTW
+
+#resource "aws_nat_gateway" "gw" {
+#  allocation_id = "${aws_eip.nat.id.*.id}"
+#  subnet_id     = "${aws_subnet.public.wp_public2_subnet.id}"
+#
+#  tags {
+#    Name = "gw NAT"
+#  }
+#}
+
+#rds subnet group
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
+  name = "wp_rds_subnetgroup"
+
+  subnet_ids = [":q
+
---
+++ terraansible/terraform.tfvars
@@ -0,0 +1,15 @@
+aws_profile = "superhero"
+aws_region  = "eu-central-1"
+vpc_cidr    = "10.0.0.0/16"
+cidrs       = {
+ public1  = "10.0.1.0/24"
+ public2  = "10.0.2.0/24"
+ private1 = "10.0.3.0/24"
+ private2 = "10.0.4.0/24"
+ private3 = "10.0.5.0/24"
+ private4 = "10.0.6.0/24"
+ private5 = "10.0.7.0/24"
+ private6 = "10.0.8.0/24"
+ oracle1  = "10.0.9.0/24"
+ oracle2  = "10.0.10.0/24"
+}
